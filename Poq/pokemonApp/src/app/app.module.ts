06c26a12c6732e8830bf2ec2836925d9f2badccd
maisa <maisasantos2003@gmail.com>
2023-11-01 20:11:09 -0300

    upload projeto

---
+++ Poq/pokemonApp/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { HttpClientModule } from '@angular/common/http'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
