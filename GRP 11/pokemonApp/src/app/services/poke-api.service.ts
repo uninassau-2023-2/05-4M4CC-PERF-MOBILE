484b45c3f2704e0a3a3b7bf26e98378ba6fda274
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-11-03 21:10:01 -0300

    add pokeapi project

---
+++ GRP 11/pokemonApp/src/app/services/poke-api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokeAPIService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getPokeAPIService(id: number = Math.floor(Math.random()*100)){
+    return this.httpClient.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  }
+}
