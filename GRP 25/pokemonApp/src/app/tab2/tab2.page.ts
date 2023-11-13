5f339037a15c57e59e6cf43dfd433f698082bfcc
Petervl80 <pedro.lapa1911@gmail.com>
2023-11-12 23:15:20 -0300

    Tab 2 page

--- GRP 25/pokemonApp/src/app/tab2/tab2.page.ts
+++ GRP 25/pokemonApp/src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PokeAPIService } from '../services/poke-api.service';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,39 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public photoSevice: PhotoService,
+     private pokeAPIService: PokeAPIService) {}
 
+    public pokemon: any = {
+      nome: '',
+      habilidades: '',
+      altura: '',
+      peso: ''
+  
+    }
+  
+    public img: string = ''
+
+    ionViewWillEnter() {
+      this.buscarPokemon();
+    }
+
+    buscarPokemon() {
+      this.pokeAPIService.getPokeAPIService().subscribe((data: any) => {
+        this.pokemon.nome = data.name
+        let index: number = 0
+        let todasHabilidades: any[] = data.abilities
+        for(let i: number = 0; i < todasHabilidades.length; i++) {
+          index =  index + 1
+        }
+        this.pokemon.habilidades = index
+        this.pokemon.altura = data.height
+        this.pokemon.peso = data.weight
+        this.img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`
+      })
+    }
+  
+    addPhotoToGallery() {
+      this.photoSevice.addNewToGallery();
+    }
 }
