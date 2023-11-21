28528ad43cb11b528c814b6139fef0107e6cd84a
Maísa <maisasantos2003@gmail.com>
2023-11-21 18:05:43 -0300

    Atualização e correção de Bugs

--- GRP 08/pokemonApp/src/app/tab1/tab1.page.ts
+++ GRP 08/pokemonApp/src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PokeAPIService } from './../services/poke-api.service';
 import { ViaCEPService } from './../services/via-cep.service';
+import { PokemonStateService } from './../services/pokemon-state.service'; // Importando o serviço
 
 @Component({
   selector: 'app-page',
@@ -26,27 +27,27 @@ export class Tab1Page {
 
   constructor(
     private pokeAPIService: PokeAPIService,
-    private viaCEPService: ViaCEPService
+    private viaCEPService: ViaCEPService,
+    private pokemonStateService: PokemonStateService // Adicionando o serviço
   ) {}
 
   buscarPokemon() {
-    this.viaCEPService.getViaCEP(this.areaBuscarPokemon)
-      .subscribe((value) => {
-        this.areaBusca.logradouro = JSON.parse(JSON.stringify(value))['logradouro'];
-        this.areaBusca.bairro = ', ' + JSON.parse(JSON.stringify(value))['bairro'];
-        this.areaBusca.localidade = ' - ' + JSON.parse(JSON.stringify(value))['localidade'];
-        this.areaBusca.uf = ' - ' + JSON.parse(JSON.stringify(value))['uf'];
-      });
+    this.viaCEPService.getViaCEP(this.areaBuscarPokemon).subscribe((value) => {
+      this.areaBusca.logradouro = JSON.parse(JSON.stringify(value))['logradouro'];
+      this.areaBusca.bairro = ', ' + JSON.parse(JSON.stringify(value))['bairro'];
+      this.areaBusca.localidade = ' - ' + JSON.parse(JSON.stringify(value))['localidade'];
+      this.areaBusca.uf = ' - ' + JSON.parse(JSON.stringify(value))['uf'];
+    });
 
-      this.pokeAPIService.getPokeAPIService()
-      .subscribe((value) => {
-        this.pokemon.name = JSON.parse(JSON.stringify(value))['name'];
-        this.pokemon.front_default = JSON.parse(JSON.stringify(value))['sprites']['front_default'];
-        this.pokemon.abilities = JSON.parse(JSON.stringify(value))['abilities'].length;
-        this.pokemon.height = JSON.parse(JSON.stringify(value))['height'];
-        this.pokemon.weight = JSON.parse(JSON.stringify(value))['weight'];
-      });
+    this.pokeAPIService.getPokeAPIService().subscribe((value) => {
+      this.pokemon.name = JSON.parse(JSON.stringify(value))['name'];
+      this.pokemon.front_default = JSON.parse(JSON.stringify(value))['sprites']['other']['dream_world']['front_default'];
+      this.pokemon.abilities = JSON.parse(JSON.stringify(value))['abilities'];
+      this.pokemon.height = JSON.parse(JSON.stringify(value))['height'];
+      this.pokemon.weight = JSON.parse(JSON.stringify(value))['weight'];
 
-
+      // Adicionando os dados do Pokémon ao serviço PokemonStateService
+      this.pokemonStateService.setUserPokemon(this.pokemon);
+    });
   }
 }
