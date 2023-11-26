9fe5966653a2eb10d1f3e5356eb5be39bb1e9b49
João Arthur Duarte de Faria <112669326+Sertoriel@users.noreply.github.com>
2023-11-26 20:07:19 -0300

    Atualização Histórico de Batalhas

--- GRP 08/pokemonApp/src/app/app.module.ts
+++ GRP 08/pokemonApp/src/app/app.module.ts
@@ -5,14 +5,14 @@ import { RouteReuseStrategy } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { PokemonStateService } from './services/pokemon-state.service'; // Importe o serviço PokemonStateService
+import { PokemonStateService } from './services/pokemon-state.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    PokemonStateService, // Adicione o PokemonStateService aos provedores
+    PokemonStateService, 
   ],
   bootstrap: [AppComponent],
 })
--- GRP 08/pokemonApp/src/app/services/pokemon-state.service.ts
+++ GRP 08/pokemonApp/src/app/services/pokemon-state.service.ts
@@ -8,13 +8,31 @@ export class PokemonStateService {
     abilities: [] // Inicialmente vazio
   };
 
-  constructor() {}
+  private battles: { won: number; lost: number; draw: number } = { won: 0, lost: 0, draw: 0 };
 
   setUserPokemon(pokemon: any) {
-    this.userPokemon = pokemon; // Configuração do Pokémon da Tab 1 no serviço
+    this.userPokemon = pokemon;
+  }
+
+  getUserPokemon() {
+    return this.userPokemon;
   }
 
   getPokemonAbilities(): number {
-    return this.userPokemon.abilities.length; // Retorna o número de habilidades do Pokémon
+    return this.userPokemon.abilities.length;
+  }
+
+  updateBattleResult(result: 'won' | 'lost' | 'draw') {
+    if (result === 'won') {
+      this.battles.won++;
+    } else if (result === 'lost') {
+      this.battles.lost++;
+    } else {
+      this.battles.draw++;
+    }
+  }
+
+  getBattleStats() {
+    return this.battles;
   }
 }
